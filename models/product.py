6e2c501fb52978b907a31ad89c81e73b2897893a
SoteWorld <prestigio27825@gmail.com>
2025-06-11 06:25:28 +0300

    Create models data

--- models/product.py
+++ models/product.py
@@ -1,9 +1,8 @@
-# models/product.py
-
 import uuid
 from pydantic import BaseModel, Field
 from typing import Optional
 from datetime import date
+
 
 class Product(BaseModel):
     """
@@ -12,5 +11,6 @@ class Product(BaseModel):
     """
     id: str = Field(default_factory=lambda: str(uuid.uuid4()), description="Уникальный идентификатор продукта (генерируется).")
     name: str = Field(..., description="Название продукта.")
+    description: Optional[str] = Field(None, description="Описание продукта.")
     expiration_date: Optional[date] = Field(None, description="Срок годности продукта.")
     ingredients: Optional[str] = Field(None, description="Состав продукта.")
--- models/store.py
+++ models/store.py
@@ -1,5 +1,3 @@
-# models/store.py
-
 import uuid
 from pydantic import BaseModel, Field
 from typing import Optional
@@ -9,7 +7,7 @@ class Store(BaseModel):
     Модель данных для представления магазина.
     ID будет генерироваться, так как на сайте его нет.
     """
-    # Теперь ID обязателен, но будет генерироваться, если не указан (например, при парсинге, если его еще нет)
+
     id: str = Field(default_factory=lambda: str(uuid.uuid4()), description="Уникальный идентификатор магазина (генерируется).")
     name: str = Field(..., description="Название магазина.")
     address: str = Field(..., description="Полный адрес магазина.")
