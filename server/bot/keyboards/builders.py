e5dead2540765521556c2d8cf670c3e4de4106dd
prest <prestigio27825@gmail.com>
2025-06-12 16:50:33 +0300

    Create logic for implementing fast api. webhook implementation. Basic logic for telegram bot and api operation. Server implemented.

---
+++ server/bot/keyboards/builders.py
@@ -0,0 +1,9 @@
+from aiogram.types import WebAppInfo
+from aiogram.utils.keyboard import InlineKeyboardBuilder
+
+from server.config_reader import config
+
+main_markup = (
+    InlineKeyboardBuilder()
+    .button(text="Open Mini App!", web_app=WebAppInfo(url=config.WEBAPP_URL))
+).as_markup()
