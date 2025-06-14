0e6946af87488a817890714a9f37f4cdbdc17e52
prest <prestigio27825@gmail.com>
2025-06-14 17:41:20 +0300

    Create DataLoader

--- services/__init__.py
+++ services/__init__.py
@@ -2,10 +2,12 @@ from .base_redis_repository import BaseRedisRepository
 from .product_repository import ProductRedisRepository
 from .store_repository import StoreRedisRepository
 from .redis_repository import RedisRepository
+from .data_loader import DataLoader
 
 __all__ = [
     "BaseRedisRepository",
     "ProductRedisRepository",
     "StoreRedisRepository",
     "RedisRepository",
+    "DataLoader",
 ]
---
+++ services/data_loader.py
@@ -0,0 +1,45 @@
+from __future__ import annotations
+
+from typing import List, Optional
+
+from parsers import ProductParser, StoreParser
+from models import Product, Store
+from .redis_repository import RedisRepository
+
+
+class DataLoader:
+    """Сервис для парса данных и сохранения их в Redis."""
+    def __init__(
+        self,
+        repository: Optional[RedisRepository] = None,
+        *,
+        product_parser: Optional[ProductParser] = None,
+        store_parser: Optional[StoreParser] = None,
+    ) -> None:
+        self.repository = repository or RedisRepository()
+        self.product_parser = product_parser or ProductParser()
+        self.store_parser = store_parser or StoreParser()
+
+    async def load_products(self, ttl: int | None = 1800) -> List[Product]:
+        """"""
+        products = await self.product_parser.parse()
+        if not products:
+            return []
+        for product in products:
+            await self.repository.add_product(product, ttl=ttl)
+        return products
+
+    async def load_stores(self, ttl: int | None = 1800) -> List[Store]:
+        stores = await self.store_parser.parse()
+        if not stores:
+            return []
+        for store in stores:
+            await self.repository.add_store(store, ttl=ttl)
+        return stores
+
+    async def load_all(self, ttl: int | None = 1800) -> None:
+        await self.load_products(ttl=ttl)
+        await self.load_stores(ttl=ttl)
+
+    async def close(self) -> None:
+        await self.repository.close()
