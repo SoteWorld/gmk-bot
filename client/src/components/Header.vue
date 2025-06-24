04a9fcfb5516202eac4c06c931bdef8ad42de72e
prest <prestigio27825@gmail.com>
2025-06-24 23:46:17 +0300

    update client

--- client/src/components/Header.vue
+++ client/src/components/Header.vue
@@ -1,10 +1,10 @@
 <template>
-  <header class="bg-red-700 text-white px-4 py-4 shadow-sm">
-    <div class="flex items-center justify-center space-x-2">
-      <Beef class="h-6 w-6 text-white" />
-      <div class="text-center">
-        <h1 class="text-base font-bold">Гомельский мясокомбинат</h1>
-        <p class="text-red-100 text-xs">Качество, проверенное временем</p>
+  <header class="header">
+    <div class="header-container">
+      <Beef class="header-icon" />
+      <div class="header-text">
+        <h1 class="header-title">Гомельский мясокомбинат</h1>
+        <p class="header-subtitle">Качество, проверенное временем</p>
       </div>
     </div>
   </header>
@@ -14,4 +14,29 @@
 import { Beef } from 'lucide-vue-next'
 </script>
 
-<style scoped></style>
+<style scoped>
+@reference utilities;
+.header {
+  @apply bg-red-700 text-white px-4 py-4 shadow-sm;
+}
+
+.header-container {
+  @apply flex items-center justify-center space-x-2;
+}
+
+.header-icon {
+  @apply h-6 w-6 text-white;
+}
+
+.header-text {
+  @apply text-center;
+}
+
+.header-title {
+  @apply text-base font-bold;
+}
+
+.header-subtitle {
+  @apply text-red-100 text-xs;
+}
+</style>
